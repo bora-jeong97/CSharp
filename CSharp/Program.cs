f78e499f2f773c744b7c38fb58076a7a8759fcf4
bora-jeong97 <purple7714@naver.com>
2023-12-26 00:39:16 +0900

    Initial commit

---
+++ CSharp/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+            // --------------------------------------------------------------
+            // 논리 연산
+            /*            bool isTall = true;
+                        bool isSmart = false;
+
+                        // AND OR NOT 
+                        // &&  ||  !
+
+                        // 키가 크면서 AND 똑똑하다
+                        bool result = (isTall && isSmart);*/
+
+
+
+
+
+
+            // --------------------------------------------------------------
+            // 비교 연산
+            /*            int hp = 100;
+                        int maxHp = 100;
+                        bool fullHp = (hp == maxHp);
+                        Console.WriteLine(fullHp);
+
+                        bool isAlive = (hp > 0); // 살아있는지 여부
+                        Console.WriteLine(isAlive);
+
+                        int level = 10;
+                        bool canEnterDungeon = (level <= 5); // 레벨 제한
+                        Console.WriteLine(canEnterDungeon);*/
+
+
+            // --------------------------------------------------------------
+            // 스트링 포멧
+            /*            string input;
+                        input = Console.ReadLine();
+                        int number = int.Parse(input);
+                        Console.WriteLine(number);
+
+                        int hp = 100;
+                        int maxHp = 200;
+                        string message = $"당신의 HP는 {hp} / {maxHp}입니다.";
+                        Console.WriteLine(message);*/
+
+        }
+    }
+}
