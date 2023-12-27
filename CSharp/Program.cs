78188e0c9b6a89b306240a76f2aa54aabbfd7e18
bora-jeong97 <purple7714@naver.com>
2023-12-28 01:15:54 +0900

    section2완료
    삭제 전 commit

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -8,6 +8,77 @@ namespace CSharp
 {
     class Program
     {
+        // --------------------------------------------------------------
+        // 연습문제 3 팩토리얼 함수 구현
+        static int Factorial(int n)
+        {
+            // 방법 2 재귀함수
+            if (n <= 1)
+                return 1;
+            return n * Factorial(n - 1);
+
+/*
+            // 방법 1
+            int ret = 1;
+
+            for (int num = 1; num <= n; num++)
+            {
+                ret *= num;
+            }
+            return ret;
+*/
+
+        }
+
+
+        // --------------------------------------------------------------
+        // 오버로딩(함수 이름의 재사용)
+        /*        static int Add(int a, int b)
+                {
+
+                    return a + b;
+                }
+                static int Add(int a, int b, int c)
+                {
+
+                    return a + b + c;
+                } 
+
+        static float Add(float a, float b)
+        {
+
+            return a + b;
+        }
+
+        static int Add(int a, int b, int c = 0, float d = 1.0f, double e = 3.0) //매개변수를 안넣을 경우의 기본값을 지정해 줄 수 있다.
+        {
+            
+            return a + b + c + (int)d + (int)e;
+        }
+        */
+        // --------------------------------------------------------------
+        // ref(복사가 아닌 진짜 참조), out(값이 필수로 들어가야함)
+        /*        static void Divide(int a, int b, out int result1, out int result2)
+                {
+                    result1 = a / b;
+                    result2 = a % b;
+                }
+        */
+
+        // --------------------------------------------------------------
+        // 함수
+        /*        static void AddOne(ref int number)
+                {
+                    number++;
+                }
+                static int AddOne2(int number)
+                {
+                    return number + 1;
+                }*/
+        /*        static int Add(int a, int b)
+                {
+                    return a + b;
+                }*/
 
         // --------------------------------------------------------------
         // 가위바위보 게임
@@ -19,8 +90,127 @@ namespace CSharp
                     Scissors = 0
                 }*/
 
+
         static void Main(string[] args)
         {
+            // --------------------------------------------------------------
+            // 연습문제 3 팩토리얼 함수 구현
+            // 5! = 5 * (4!)
+            // 5! = 5 * 4 * 3 * 2 * 1
+            // n! = n * (n-1) * ... * 1 (n >= 1)
+            int ret = Factorial(5);
+            Console.WriteLine(ret);
+            // --------------------------------------------------------------
+            // 연습문제 2 별찍기
+
+            /*            for (int i = 1; i <= 5; i++)
+                        {
+                            for (int j = 0; j < i; j++)
+                            {
+                                Console.Write("*");
+                            }
+                            Console.WriteLine();
+                        }*/
+
+            // --------------------------------------------------------------
+            // 연습문제 1 구구단
+            /*            for (int i = 2; i <= 9; i++)
+                        {
+                            for (int j = 1; j <= 9; j++)
+                            {
+                                Console.WriteLine($"{i} X {j} = {i * j}");
+                            }
+                        }*/
+
+
+
+            // --------------------------------------------------------------
+            // 오버로딩(함수 이름의 재사용)
+
+            /*            int result = Add(1, 2, 3);
+                        Console.WriteLine(result);*/
+
+            // --------------------------------------------------------------
+            // ref, out
+            /*            int num1 = 10;
+                        int num2 = 3;
+                        int result1;
+                        int result2;
+
+                        Divide(num1, num2, out result1, out result2);
+                        Console.WriteLine(result1);
+                        Console.WriteLine(result2);
+
+
+                        int a = 0;
+                        AddOne(ref a);  // 참조(진퉁)
+                        Console.WriteLine(a);
+                        a = AddOne2(a); // 복사(짭퉁)  
+                        Console.WriteLine(a);*/
+
+            // --------------------------------------------------------------
+            // 함수
+
+
+
+            /*            int result = Program.Add(3, 5);
+                        Console.WriteLine(result);*/
+
+
+            // --------------------------------------------------------------
+            // break, continue
+            // 소수파악
+            /*
+                        for (int i = 1; i <= 100; i++)
+                        {
+                            if ((i % 3) != 0)
+                                continue;
+                            Console.WriteLine($"3으로 나뉘는 숫자 : {i}");
+                        }
+
+                        int num = 10002;
+
+                        bool isPrime = true;
+
+                        for (int i = 2; i < num; i++)
+                        {
+                            if((num % i) == 0)
+                            {
+                                isPrime = false;
+
+                                break;
+                            }
+                        }
+
+                        if(isPrime)
+                            Console.WriteLine("소수입니다");
+                        else
+                            Console.WriteLine("소수가 아닙니다");*/
+
+            // --------------------------------------------------------------
+            // for 반복문
+            /*            for (int i = 0; i <1; i++) {
+                            Console.WriteLine(i);
+                        }*/
+
+
+            // --------------------------------------------------------------
+            // while 반복문
+            /*            int count = 5;
+
+                        while(count > 0)
+                        {
+                            Console.WriteLine(count);
+                            count--;
+                        }
+                        string answer;
+                        do
+                        {
+                            Console.WriteLine("강사님은 잘 생기셨나요? (y/n) :");
+                            answer = Console.ReadLine();
+                        } while (answer != "y");
+
+                        Console.WriteLine("정답입니다");*/
 
 
             // --------------------------------------------------------------
