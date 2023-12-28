d2a036f4bf99b56d96f8b318ed2bf2d9effd8428
bora-jeong97 <purple7714@naver.com>
2023-12-28 23:22:31 +0900

    static

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -16,8 +16,44 @@ namespace CSharp
     // class(Ref 참조) 
     class Knight    // 객체묘사 class ex) 설계도, 붕어빵 틀
     {
+        // 필드
+        static public int counter = 1; // static 오로지 1개만 존재
+
+        public int id;
         public int hp;
         public int attack;
+
+        static public Knight CreateKnight()
+        {
+            Knight knight = new Knight();
+            knight.hp = 100;
+            knight.attack = 1;
+            return knight;
+        }
+
+
+        public Knight()    // 생성자 반환값 없음. class와 명이 같아야함
+        {
+            id = counter;
+            counter++;
+
+            hp = 100;
+            attack = 10;
+            Console.WriteLine("생성자 호출");
+        }
+
+        public Knight(int hp) : this()  // this() = Knight(), 즉 상단의 Knight()를 먼저 호출 후 실행됨 
+        {
+            this.hp = hp;
+            Console.WriteLine("int 생성자 호출");
+        }
+
+        public Knight(int hp, int attack)   
+        {
+            this.hp = hp;
+            this.attack = attack;
+            Console.WriteLine("int, int 생성자 호출");
+        }
 
         public Knight Clone()
         {
@@ -60,22 +96,9 @@ namespace CSharp
 
         static void Main(string[] args)
         {
+            Knight knight = Knight.CreateKnight();  // static 함수 : 클래스에서 바로 접근가능
+            knight.Move();  // 일반 함수 : 객체를 만든뒤에 접근가능
 
-            Mage mage;
-            mage.hp = 100;
-            mage.attack = 50;
-            KillMage(mage);
-
-            Mage mage2 = mage;
-            mage2.hp = 0;
-
-            Knight knight = new Knight();
-            knight.hp = 100;
-            knight.attack = 10;
-            
-            
-            Knight knight2 = knight.Clone();    // 기존 값과 분리된 클론 객체
-            knight2.hp = 0;
 
 
 
