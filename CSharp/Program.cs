c71fdcf0e941c60716433ec0fea0443ad1b6c92a
bora-jeong97 <purple7714@naver.com>
2023-12-28 23:59:39 +0900

    상속성

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -6,40 +6,75 @@ using System.Threading.Tasks;
 
 namespace CSharp
 {
-    // 객체(OOP Object Oriented Programming) 지향
+    // 객체(OOP Object Oriented Programming) 지향 (은닉성/상속성/다형성)
 
     // Knight
     // 속성 : hp, attack, pos(좌표)
     // 기능 : Move, Attack, Die
 
-
-    // class(Ref 참조) 
-    class Knight    // 객체묘사 class ex) 설계도, 붕어빵 틀
+    class Player    // 부모/기반 클래스
     {
-        // 필드
         static public int counter = 1; // static 오로지 1개만 존재
-
         public int id;
         public int hp;
         public int attack;
 
-        static public Knight CreateKnight()
+        public Player()
+        {
+            Console.WriteLine("Player 생성자 호출");
+        }
+
+        public Player(int hp)
+        {
+            this.hp = hp;
+            Console.WriteLine("Player hp 생성자 호출!");
+        }
+
+        public void Move()
+        {
+            Console.WriteLine("Player Move");
+        }
+
+        public void Attack()
+        {
+            Console.WriteLine("Player Attack");
+
+        }
+
+    }
+
+    
+
+
+
+    class Mage : Player // : Player 상속. 자식/파생 클래스
+    {
+
+    }
+
+    class Archer : Player
+    {
+
+    }
+
+
+
+    // class(Ref 참조) 
+    class Knight : Player    // 객체묘사 class ex) 설계도, 붕어빵 틀
+    {
+        public Knight Clone()
         {
             Knight knight = new Knight();
-            knight.hp = 100;
-            knight.attack = 1;
+            knight.hp = hp;
+            knight.attack = attack;
             return knight;
         }
 
 
-        public Knight()    // 생성자 반환값 없음. class와 명이 같아야함
-        {
-            id = counter;
-            counter++;
 
-            hp = 100;
-            attack = 10;
-            Console.WriteLine("생성자 호출");
+        public Knight()  // 생성자 반환값 없음. class와 명이 같아야함. : base(100) 부모클래스 접근. base(100) == Player(100) 이 메소드가 호출되면 ()의 기본 생성자는 호출 안됨.
+        {
+            Console.WriteLine("Knight 생성자 호출");
         }
 
         public Knight(int hp) : this()  // this() = Knight(), 즉 상단의 Knight()를 먼저 호출 후 실행됨 
@@ -55,32 +90,9 @@ namespace CSharp
             Console.WriteLine("int, int 생성자 호출");
         }
 
-        public Knight Clone()
-        {
-            Knight knight = new Knight();
-            knight.hp = hp;
-            knight.attack = attack;
-            return knight;
-        }
-
-        public void Move()
-        {
-            Console.WriteLine("Knight Moce");
-        }
-
-        public void Attack()
-        {
-            Console.WriteLine("Knight Attack");
-
-        }
     }
 
-    // struct(복사)
-    struct Mage
-    {
-        public int hp;
-        public int attack;
-    }
+
 
     class Program
     {
@@ -96,11 +108,9 @@ namespace CSharp
 
         static void Main(string[] args)
         {
-            Knight knight = Knight.CreateKnight();  // static 함수 : 클래스에서 바로 접근가능
-            knight.Move();  // 일반 함수 : 객체를 만든뒤에 접근가능
+            Knight knight = new Knight();  // static 함수 : 클래스에서 바로 접근가능
 
-
-
+            knight.Move();
 
         }
     }
