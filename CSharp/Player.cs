812b2ae767ed2973a2bcd1ad5190d66d948b579a
bora-jeong97 <purple7714@naver.com>
2023-12-30 04:20:26 +0900

    TextRPG2 플레이어 생성

---
+++ CSharp/Player.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    public enum PlayerType
+    {
+        None = 0,
+        Knight = 1,
+        Archer = 2,
+        Mage = 3
+    }
+
+
+    class Player
+    {
+        protected PlayerType type = PlayerType.None;
+        protected int hp = 0;
+        protected int attack = 0;
+
+
+        protected Player(PlayerType type)  // 매개변수 필수. 기본 생성자는 안만들었기 때문에 무조건 type을 넣어야 사용가능하다.
+        {
+            this.type = type;
+        }
+
+        public void SetInfo(int hp, int attack)
+        {
+            this.hp = hp;
+            this.attack = attack;
+        }
+
+        public int GetHp() { return hp; }
+        public int GetAttack() { return attack; }
+
+    }
+
+    class Knight : Player
+    {
+        public Knight() : base(PlayerType.Knight)
+        {
+            SetInfo(100, 10);
+        }
+
+    }
+
+    class Archer : Player
+    {
+        public Archer() : base(PlayerType.Archer)
+        {
+            SetInfo(75, 12);
+        }
+    }
+
+    class Mage : Player
+    {
+        public Mage() : base(PlayerType.Mage)
+        {
+            SetInfo(50, 15);
+        }
+    }
+}
--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -7,44 +7,14 @@ using System.Threading.Tasks;
 namespace CSharp
 {
  
-    class Player
+    
+    class Program
     {
 
         static void Main(string[] args)
         {
-            string name = "Harry Potter";
-            
-            
-            // 1. 찾기
-            bool find = name.Contains("Harry"); // True
-            int index = name.IndexOf('o');  // 7
 
-            // 2. 변형
-            name = name + "Junior"; // Harry PotterJunior
-            string lowerCaseNmae = name.ToLower();  // harry potterjunior
-            string upperCaseNmae = name.ToUpper();  // HARRY POTTERJUNIOR
-            string replaceName = name.Replace('t', 'k');    // Harry PokkerJunior
-
-
-            // 3. 분할
-            string[] names = name.Split(new char[] { ' ' });
-            string substringNmae = name.Substring(4);   // y PotterJunior
-
-            for (int i = 0; i < names.Length; i++)
-            {
-                Console.WriteLine(names[i]);
-            }
-/*
-            Console.WriteLine(find);
-            Console.WriteLine(index);
-            Console.WriteLine(name);
-            Console.WriteLine(lowerCaseNmae);
-            Console.WriteLine(upperCaseNmae);
-            Console.WriteLine(replaceName);
-            Console.WriteLine(names);
-            Console.WriteLine(substringNmae);*/
-            
-
+            Player player = new Player();
         }
     }
 }
