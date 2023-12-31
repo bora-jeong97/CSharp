7e4eb6ae87a40d55edfb1d5847d1b8c57ec40dac
bora-jeong97 <purple7714@naver.com>
2023-12-31 22:15:49 +0900

    배열의 크기가 변동되는 경우

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -11,44 +11,15 @@ namespace CSharp
     // 이차원 배열
     class Program
     {
-        class Map
-        {
-            int[,] tiles =
-            {
-                { 1, 1, 1, 1, 1},
-                { 1, 0, 0, 0, 1},
-                { 1, 0, 0, 0, 1},
-                { 1, 0, 0, 0, 1},
-                { 1, 1, 1, 1, 1},
 
-            };
-
-            public void Render()
-            {
-                var defaultColor = Console.ForegroundColor;
-
-                for (int i = 0; i < tiles.GetLength(1); i++)
-                {
-                    for (int j = 0; j < tiles.GetLength(0); j++)
-                    {
-                        if (tiles[i, j] == 1)
-                            Console.ForegroundColor = ConsoleColor.Red; // 색 변경
-                        else
-                            Console.ForegroundColor = ConsoleColor.Green; // 색 변경
-
-                        Console.Write('\u25cf');    // 타원형 
-                    }
-                    Console.WriteLine();
-                }
-
-                Console.ForegroundColor = defaultColor; // 마지막 색을 디폴트로 다시 바꿔주기 위함
-            }
-        }
 
         static void Main(string[] args)
         {
-            Map map = new Map();
-            map.Render();
+            int[][] a = new int[2][];
+            a[0] = new int[3];
+            a[0] = new int[6];
+            a[0] = new int[2];
+
         }
     }
 }
