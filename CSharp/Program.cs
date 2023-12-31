e98a2757c7cf3cf9f11f9631ad2d8b6fa85d14da
bora-jeong97 <purple7714@naver.com>
2023-12-31 22:43:53 +0900

    자료구조_리스트

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,24 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSharp
 {
 
     // 자료구조
-    // 이차원 배열
+    // 리스트(List) <- 동적 배열 
     class Program
     {
 
-
+        // 배열과는 달리 만들때 크기가 고정되지 않는다.
         static void Main(string[] args)
         {
-            int[][] a = new int[2][];
-            a[0] = new int[3];
-            a[0] = new int[6];
-            a[0] = new int[2];
+            // List <- 동적배열
+            // class 형이기 때문에 참조형이다.
+            List<int> list = new List<int>();
+
+            // 뒤에 추가
+            for (int i = 0; i < 5; i++)
+            {
+                list.Add(i);    // 뒤에 추가
+            }
+    
+            // 삽입
+            list.Insert(2, 999); // 지정 인덱스에 요소를 삽입
+            
+            // 삭제
+            list.RemoveAt(0); // 인덱스로 삭제
+
+
+            list.Remove(3); // 값으로 삭제
+
+            // 전체 삭제
+            list.Clear();
+
+            // 출력
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(i);
+            }
 
         }
     }
