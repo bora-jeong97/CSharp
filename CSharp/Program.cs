8e046064a16de8c23642afcabba95a23e6b01bfe
bora-jeong97 <purple7714@naver.com>
2024-01-01 17:49:47 +0900

    Interface 인터페이스

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -4,53 +4,49 @@ using System.Collections.Generic;
 namespace CSharp
 {
 
-
+    // 추상 클래스
     class Program
     {
-        // 클래스의 일반화
-        // 일반화 형식 : 어떤 형식을 넣어도 사용 가능
-        class MyList<T> // 제네릭/일반화 클래스
+
+        abstract class Monster  // abstract 추상클래스 : 개념적으로만 존재 인스턴스를 만들 수 없음. 상속받는 자식클래스는 무조건 부모클래스의 함수를 구현해야함.
         {
+            public abstract void Shout();
+        }
 
-            T[] arr = new T[10];
+        interface IFlyable
+        {
+            void Fly();
 
-            public T GetItem(int i)
+        }
+
+        class Orc : Monster
+        {
+            public override void Shout()
             {
-                return arr[i];
+                Console.WriteLine("록타르 오가르!");
             }
         }
 
-        class Monster{}
-
-        // 함수의 일반화
-        static void Test<T>(T input)
+        class FlyableOrc : Orc, IFlyable
         {
-
+            public void Fly()
+            {
+            }
         }
 
-        // 함수 인자가 여러개 일 떄
-        static void Test2<T, K>(T input, K input2)
+        static void DoFly(IFlyable flayable)
         {
-
+            flayable.Fly(); 
         }
 
-        // 조건을 주는 경우
-        static void Test3<T, K>(T input) where T : struct // where T : struct(값형식), class(참조형식), new(어떠한 인자도 받지않는 기본생성자), Monster(몬스터 혹은 몬스터를 상속받는 것)
-        {
 
-        }
+
 
         static void Main(string[] args) {
 
-            MyList<int> myIntList = new MyList<int>();
-            int item = myIntList.GetItem(0);
-            MyList<short> mySortList = new MyList<short>();
-            MyList<Monster> myMonsterLList = new MyList<Monster>();
-
-            Test<int>(3);
-            Test<float>(3.0f);
-
-            Test2<int, string>(1, "아무거나 넣어도 된다.");
+            IFlyable orc = new FlyableOrc();
+            DoFly(orc); // IFlyable을 내포한 클래스만 매개변수로 사용가능
+                       
         }
     }
 }
