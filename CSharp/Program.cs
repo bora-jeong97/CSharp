1eaf111f677b7974766c8fe5107f4cdc35182289
bora-jeong97 <purple7714@naver.com>
2024-01-03 00:15:46 +0900

    Reflection(리플렉션)

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,40 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace CSharp
 {
-    // 예외처리
+
+
+    // 리플렉션 Reflection : X-Ray 
     class Program
     {
-        class TestException : Exception
+        class Monster
         {
+            public int hp;
+            protected int attack;
+            private float speed;
 
+            void Attack() { }
         }
-
 
         static void Main(string[] args)
         {
+            
+            Monster monster = new Monster();
+            Type type = monster.GetType();
 
-            try
-            {
-                // 예외상황
-                // 1. 0으로 나눌 때
-                // 2. 잘못된 메모리를 참조(null)
-                // 3. 오버플로우
-                throw new TestException();
-            }
-            catch (DivideByZeroException e)
-            {
+            var fields = type.GetFields(System.Reflection.BindingFlags.Public
+                | System.Reflection.BindingFlags.NonPublic
+                | System.Reflection.BindingFlags.Static
+                | System.Reflection.BindingFlags.Instance
+                );
 
-            }
-            catch (Exception e)
+            foreach (FieldInfo field in fields)
             {
+                string access = "protected";
+                if (field.IsPublic)
+                    access = "public";
+                else if (field.IsPrivate)
+                    access = "private";
 
+                Console.WriteLine($"{access} {field.FieldType.Name} {field.Name}");
             }
-            finally // 에러가 발생 됐음에도 무조건 실행해야할 때 ex) DB, 파일 정리 등
-            {
 
-            }
         }
     }
 }
