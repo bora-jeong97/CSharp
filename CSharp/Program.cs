6341ee21d29a36d4ccdfa3018c4f953df6b18ea3
bora-jeong97 <purple7714@naver.com>
2024-01-02 23:01:30 +0900

    람다식

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -34,14 +34,7 @@ namespace CSharp
 
         static List<Item> _items = new List<Item>();
 
-        delegate bool ItemSelector(Item item);
-
-        static bool IsWeapon(Item item)
-        {
-            return item.ItemType == ItemType.Weapon;
-        }
-
-        static Item FindWeapon(ItemSelector selector)
+        static Item FindItem(Func<Item, bool> selector) // 아이템 찾기
         {
             foreach(Item item in _items)
             {
@@ -60,7 +53,18 @@ namespace CSharp
             _items.Add(new Item() { ItemType = ItemType.Armor, Rarity = Rarity.Uncommon });
             _items.Add(new Item() { ItemType = ItemType.Ring, Rarity = Rarity.Rare });
 
-            Item item = FindWeapon(IsWeapon);
+            // delegate를 직접 선언하지 않아도, 이미 만드러진 애들이 존재한다.
+            // -> 반환 타입이 있을 경우 Func
+            // -> 반환 타입이 없으면 Action
+            
+            
+            // Anonoymous Function : 무명 함수 / 익명 함수 
+            // 방법 1. delegate 일회성
+            //Item item = FindItem(delegate (Item itm){ return itm.ItemType == ItemType.Weapon; });
+            // 방법 2. 람다식
+            Item item = FindItem((Item itm) => { return itm.ItemType == ItemType.Weapon; });
+
+
         }
     }
 }
