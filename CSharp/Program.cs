9ee0573b20db0e2db87252d537558b36502c9ca7
bora-jeong97 <purple7714@naver.com>
2024-01-02 23:26:47 +0900

    Exception 예외처리

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -3,68 +3,38 @@ using System.Collections.Generic;
 
 namespace CSharp
 {
-
-
-    enum ItemType
-    {
-        Weapon, 
-        Armor,
-        Amulet,
-        Ring
-    }
-
-    enum Rarity
-    {
-        Normal,
-        Uncommon,
-        Rare,
-    }
-
-    class Item
-    {
-        public ItemType ItemType;
-        public Rarity Rarity;
-    
-    }
-
-
-    // Lambda : 일회용 함수를 만드는데 사용하는 문법이다.
+    // 예외처리
     class Program
     {
-
-        static List<Item> _items = new List<Item>();
-
-        static Item FindItem(Func<Item, bool> selector) // 아이템 찾기
+        class TestException : Exception
         {
-            foreach(Item item in _items)
-            {
-                if (selector(item)) // true인경우 item을 리턴
-                    return item;
-            }
-            return null;
-        }
 
+        }
 
 
         static void Main(string[] args)
         {
 
-            _items.Add(new Item() { ItemType = ItemType.Weapon, Rarity = Rarity.Normal });
-            _items.Add(new Item() { ItemType = ItemType.Armor, Rarity = Rarity.Uncommon });
-            _items.Add(new Item() { ItemType = ItemType.Ring, Rarity = Rarity.Rare });
+            try
+            {
+                // 예외상황
+                // 1. 0으로 나눌 때
+                // 2. 잘못된 메모리를 참조(null)
+                // 3. 오버플로우
+                throw new TestException();
+            }
+            catch (DivideByZeroException e)
+            {
 
-            // delegate를 직접 선언하지 않아도, 이미 만드러진 애들이 존재한다.
-            // -> 반환 타입이 있을 경우 Func
-            // -> 반환 타입이 없으면 Action
-            
-            
-            // Anonoymous Function : 무명 함수 / 익명 함수 
-            // 방법 1. delegate 일회성
-            //Item item = FindItem(delegate (Item itm){ return itm.ItemType == ItemType.Weapon; });
-            // 방법 2. 람다식
-            Item item = FindItem((Item itm) => { return itm.ItemType == ItemType.Weapon; });
+            }
+            catch (Exception e)
+            {
 
+            }
+            finally // 에러가 발생 됐음에도 무조건 실행해야할 때 ex) DB, 파일 정리 등
+            {
 
+            }
         }
     }
 }
