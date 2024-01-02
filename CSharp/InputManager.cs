fe91945973c665b84b9524a3d028527bbb74cd4e
bora-jeong97 <purple7714@naver.com>
2024-01-02 19:44:25 +0900

    Ovsercer Pattern (Delegate, Event)

---
+++ CSharp/InputManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Ovsercer Pattern : 구독자를 모집후 특정 이벤트 발생시 구독자들에게 메세지 발송
+namespace CSharp
+{
+    class InputManager
+    {
+        public delegate void OnInputKey(); // dalegate : 함수자체를 인자로 넘기고 싶을 때 유용
+        public event OnInputKey InputKey; // event는 delegate의 외부 접근을 막기 위해 delegate를 한번더 래핑한 것 
+
+        public void Update()
+        {
+            if (Console.KeyAvailable == false)  // 키 입력
+                return;
+
+            ConsoleKeyInfo info = Console.ReadKey();
+            if(info.Key == ConsoleKey.A)
+            {
+                // A입력 사실을 모두한테 알린다.
+                InputKey();
+            }
+        }
+    }
+}
--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -4,45 +4,26 @@ using System.Collections.Generic;
 namespace CSharp
 {
 
-    // delegate (대리자)
+    // 이벤트 ( delegate를 래핑하는 문법 접근제한을 걸기위해)
     class Program
     {
 
-        // 업체 사장 ~ 사장님의 비서
-        // 우리의 연락처/용건 ->
-        // 거꾸로 연락을 달라고 함.
 
-
-        delegate int OnClicked(); // 이것은 함수가 아닌 형식
-        // delegate -> 형식은 형식인데, 함수 자체를 인자로 넘겨주는 그런 형식
-        // 반환 int 입력 void
-        // OnClicked가 delegate 형식의 이름이다.
-
-        // UI
-        static void ButtonPressed(OnClicked clickedFunction) // 함수 자체를 인자로 넘겨주고 함수를 호출
+        static void OnInputTest()
         {
-            clickedFunction();
-        }
-
-
-        static int TestDelegate()
-        {
-            Console.WriteLine("Hello Delegate");
-            return 0;
-        }
-
-        static int TestDelegate2()
-        {
-            Console.WriteLine("Hello Delegate2");
-            return 0;
+            Console.WriteLine("Input Received");
         }
 
         static void Main(string[] args) {
 
-            OnClicked clicked = new OnClicked(TestDelegate);
-            clicked += TestDelegate2;
+            InputManager inputManager = new InputManager();
 
-            ButtonPressed(clicked);
+            inputManager.InputKey += OnInputTest;
+
+            while (true)
+            {
+                inputManager.Update();
+            }
         }
     }
 }
